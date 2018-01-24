f32cedf0bc9481e562793e78e8582dd00956f82e
Quinn McPhail <quinnmcphail@me.com>
2018-01-24 02:27:54 -0500

    Added a1

--- a1/index.php
+++ a1/index.php
@@ -21,16 +21,16 @@
 	<div class="form-container">
 
 	<form action="<?php echo 'URL_ROOT' . '/add_products_to_inventory.php' ?>" method="post">
-    
+
     <?php
 		//create a loop that writes a form to allow up to 8 produce items to be added
-	
+
 		for ($x = 0; $x <= 7; $x++) {
 	?>
     <fieldset>
 		<legend>Product Details: Product <?php echo $x ?></legend>
 			<div><label for="productname<?php echo $x ?>">Product Name</label> <input id="productname<?php echo $x ?>" type="text" name="productname<?php echo $x ?>" value="" /></div>
-			
+
 			<div>
 				<label for="producetype<?php echo $x ?>">Produce Type</label>
 				<select id="producetype<?php echo $x ?>" name="producetype<?php echo $x ?>">
@@ -40,7 +40,7 @@
 					</optgroup>
 				</select>
 			</div>
-            
+
             <div>
 				<label for="pricetype<?php echo $x ?>">Pricing Type</label>
 				<select id="pricetype<?php echo $x ?>" name="pricetype<?php echo $x ?>">
@@ -50,17 +50,17 @@
 					</optgroup>
 				</select>
 			</div>
-            
+
 			<div><label for="productprice<?php echo $x ?>">Product Price</label> $<input id="productprice<?php echo $x ?>" name="productprice<?php echo $x ?>" type="text" size="10" /></div>
 	</fieldset>
-    
-    <?php		
-			
-	)
+
+    <?php
+
+		}
 	?>
-	
-	
-    
+
+
+
 	<div class="buttonrow">
 		<input type="submit" value="Save" class="button" />
 
@@ -68,9 +68,9 @@
 	</div>
 
 	</form>
-	
+
 	</div><!-- /form-container -->
-	
+
 </div><!-- /wrapper -->
 
 
