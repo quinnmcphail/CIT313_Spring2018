e263516e3dfa24e7221f9110b0923314198de4da
Quinn McPhail <quinnmcphail@me.com>
2018-01-24 03:11:54 -0500

    Added a1

--- a1/view_products.php
+++ a1/view_products.php
@@ -20,54 +20,54 @@
 <div id="wrapper">
 
 	<h2>Please select the quantity of items you would like to put into your Shopping Cart</h2>
-  
+
 
 	<div class="form-container">
 
 	<form action="<?php echo URL_ROOT . '/add_products_to_cart.php' ?>" method="post">
-    
+
     <?php
 		//create a loop that writes a form that displays all of the products in the SESSION inventory array
-		
+
 		$aryProductArray = unserialize($_SESSION['aryProductArray']);
-	
-	
+
+
 		for ($x = 0; $x < count($arryProductArray); $x++) {
 	?>
     <fieldset>
 		<legend>Product Details</legend>
-			
+
             <?php
 				//pull the next object out of the array
 				$thisProduceItem = $aryProductArray[$x];
 			?>
-            
+
             <div>
             	<b><?php echo $thisProduceItem->product_name ?></b><br />
                 Price: $<?php echo $thisProduceItem->product_price ?>
-                
+
                 <?php
 					switch ($thisProduceItem->price_type) {
 						case 1:
 							echo "/per pound";
-							break;	
+							break;
 						case 2:
 							echo "/each";
 							break;
-					
+					}//##Added closing bracket
 				?>
 			</div>
-            
+
 			<div><label for="quantity<?php echo $x ?>">Quantity Requested</label> <input id="quantity<?php echo $x ?>" name="quantity<?php echo $x ?>" type="text" size="10" /></div>
 	</fieldset>
-    
-    <?php		
-			
+
+    <?php
+
 		}
 	?>
-	
-	
-    
+
+
+
 	<div class="buttonrow">
 		<input type="submit" value="Save" class="button" />
 
@@ -75,9 +75,9 @@
 	</div>
 
 	</form>
-	
+
 	</div><!-- /form-container -->
-	
+
 </div><!-- /wrapper -->
 
 
