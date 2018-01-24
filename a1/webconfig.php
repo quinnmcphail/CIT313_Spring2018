81745434cfb30548b89ae1457afeec623ff4c66c
Quinn McPhail <quinnmcphail@me.com>
2018-01-24 02:55:53 -0500

    Added a1

--- a1/webconfig.php
+++ a1/webconfig.php
@@ -1,4 +1,4 @@
 <?php
 
 define('ABSOLUTE_PATH', './CIT313/SP2018/a1/');
-define('URL_ROOT', 'corsair.cs.iupui.edu:22231/CIT313/SP2018/a1');
+define('URL_ROOT', 'http://corsair.cs.iupui.edu:22231/CIT313/SP2018/a1');
