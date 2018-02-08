d56dc92b197e88c813ee6ca4ab1ef9738cfa0550
Quinn McPhail <quinnmcphail@me.com>
2018-02-08 16:35:46 -0500

    Added form

--- ex3/index.php
+++ ex3/index.php
@@ -1,37 +1,48 @@
 <?php
-    ini_set('display_errors',1);
-    error_reporting(E_ALL);
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
-    function loadClasses($class){
-        include_once('classes/'.strtolower($class).'.class.php');
-    }
+function loadClasses($class)
+{
+    include_once 'classes/' . strtolower($class) . '.class.php';
+}
 
-    spl_autoload_register('loadClasses');
+spl_autoload_register('loadClasses');
 
-    $registeredUser = new RegisteredUser('user', 'Regular User');
-    $admin = new Admin('qmcphail','Administrator');
+$registeredUser = new RegisteredUser('user', 'Regular User');
+$admin = new Admin('qmcphail', 'Administrator');
 
-    $registeredUser->first_name = 'User';
-    $registeredUser->last_name = 'Name';
-    $registeredUser->email_address = 'user@name.com';
+$registeredUser->first_name = 'User';
+$registeredUser->last_name = 'Name';
+$registeredUser->email_address = 'user@name.com';
 
-    $admin->first_name = 'Quinn';
-    $admin->last_name = 'McPhail';
-    $admin->email_address = 'quinn@mcphail.com';
+$admin->first_name = 'Quinn';
+$admin->last_name = 'McPhail';
+$admin->email_address = 'quinn@mcphail.com';
 
-    echo "User Level: " . $registeredUser->user_level . "<br>";
-    echo "Registered User ID: " . $registeredUser->user_id . "<br>";
-    echo "Registered User Type: " . $registeredUser->user_type . "<br>";
-    echo "Registered First Name: " . $registeredUser->first_name . "<br>";
-    echo "Registered Last Name: " . $registeredUser->last_name . "<br>";
-    echo "Registered Email Address: " . $registeredUser->email_address . "<br><hr>";
+echo "User Level: " . $registeredUser->user_level . "<br>";
+echo "Registered User ID: " . $registeredUser->user_id . "<br>";
+echo "Registered User Type: " . $registeredUser->user_type . "<br>";
+echo "Registered First Name: " . $registeredUser->first_name . "<br>";
+echo "Registered Last Name: " . $registeredUser->last_name . "<br>";
+echo "Registered Email Address: " . $registeredUser->email_address . "<br><hr>";
 
-    echo "User Level: " . $admin->user_level . "<br>";
-    echo "Admin User ID: " . $admin->user_id . "<br>";
-    echo "Admin User Type: " . $admin->user_type . "<br>";
-    echo "Admin First Name: " . $admin->first_name . "<br>";
-    echo "Admin Last Name: " . $admin->last_name . "<br>";
-    echo "Admin Email Address: " . $admin->email_address . "<br>";
+echo "User Level: " . $admin->user_level . "<br>";
+echo "Admin User ID: " . $admin->user_id . "<br>";
+echo "Admin User Type: " . $admin->user_type . "<br>";
+echo "Admin First Name: " . $admin->first_name . "<br>";
+echo "Admin Last Name: " . $admin->last_name . "<br>";
+echo "Admin Email Address: " . $admin->email_address . "<br><hr>";
 
-    echo "Math Function: " . Admin::mathFunction(4,6) . "<br>";
+echo "Math Function: " . Admin::mathFunction(4, 6) . "<br>";
 ?>
+<html>
+<body>
+<form action="results.php" method="post">
+First Name: <input type="text" name="firstName"><br>
+Last Name: <input type="text" name="lastName"><br>
+Email Address: <input type="email" name="email"><br>
+<input type="submit">
+</form>
+</body>
+</html>
