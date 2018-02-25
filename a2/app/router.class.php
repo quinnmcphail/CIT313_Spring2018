4d1dc58b03de27c444817144b0ad3b1679e7dc75
Quinn McPhail <quinnmcphail@me.com>
2018-02-25 16:12:43 -0500

    Changed to autoLoad

--- a2/app/router.class.php
+++ a2/app/router.class.php
@@ -3,12 +3,15 @@
     function loadClasses($class)
     {
         foreach(glob(strtolower($class).'class.php') as $filename){
+            include $filename;
             echo $filename;
         }
         foreach(glob('controllers/'.strtolower($class).'class.php') as $filename){
+            include $filename;
             echo $filename;
         }
         foreach(glob('models/'.strtolower($class).'class.php') as $filename){
+            include $filename;
             echo $filename;
         }
     }
