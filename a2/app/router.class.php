36cacfddedb2c992bb1f66fc0e7797c01b595ccd
Quinn McPhail <quinnmcphail@me.com>
2018-02-25 16:10:59 -0500

    Changed to autoLoad

--- a2/app/router.class.php
+++ a2/app/router.class.php
@@ -5,6 +5,12 @@
         foreach(glob(strtolower($class).'class.php') as $filename){
             echo $filename;
         }
+        foreach(glob('controllers/'.strtolower($class).'class.php') as $filename){
+            echo $filename;
+        }
+        foreach(glob('models/'.strtolower($class).'class.php') as $filename){
+            echo $filename;
+        }
     }
 
     spl_autoload_register('loadClasses');
