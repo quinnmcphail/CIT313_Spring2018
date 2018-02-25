a3f6fafb291aaf213be50d642b585ede214c4c5a
Quinn McPhail <quinnmcphail@me.com>
2018-02-25 16:07:05 -0500

    Added folders for models and controllers

--- a2/app/router.class.php
+++ a2/app/router.class.php
@@ -1,7 +1,4 @@
 <?php
-    require_once 'app/load.class.php';
-    require_once 'app/models/model.class.php';
-    require_once 'app/controllers/controller.class.php';
 
     function loadClasses($class)
     {
