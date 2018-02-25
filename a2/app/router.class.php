3d2cf7d861376f555fd094eda59e9ea0c51f0636
Quinn McPhail <quinnmcphail@me.com>
2018-02-25 16:42:49 -0500

    Changed to autoLoad

--- a2/app/router.class.php
+++ a2/app/router.class.php
@@ -11,30 +11,6 @@
         if(file_exists('app/controllers/'.strtolower($class).'.class.php')){
             include 'app/controllers/'.strtolower($class).'.class.php';
         }
-        // foreach(glob("app/*.class.php") as $filepath){
-        //     $file = explode("/",$filepath);
-        //     $className = explode(".",$file[count($file)-1]);
-        //     $className = $className[0];
-        //     if(strtolower($class)==$className){
-        //         include $filepath;
-        //     }
-        // }
-        // foreach(glob("app/controllers/*.class.php") as $filepath){
-        //     $file = explode("/",$filepath);
-        //     $className = explode(".",$file[count($file)-1]);
-        //     $className = $className[0];
-        //     if(strtolower($class)==$className){
-        //         include $filepath;
-        //     }
-        // }
-        // foreach(glob("app/models/*.class.php") as $filepath){
-        //     $file = explode("/",$filepath);
-        //     $className = explode(".",$file[count($file)-1]);
-        //     $className = $className[0];
-        //     if(strtolower($class)==$className){
-        //         include $filepath;
-        //     }
-        // }
     }
 
     spl_autoload_register('loadClasses');
