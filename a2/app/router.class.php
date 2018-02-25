b9e02a0c07defc8e85eb453e048deb9c3c73639e
Quinn McPhail <quinnmcphail@me.com>
2018-02-25 16:41:30 -0500

    Changed to autoLoad

--- a2/app/router.class.php
+++ a2/app/router.class.php
@@ -2,30 +2,39 @@
 
     function loadClasses($class)
     {
-        foreach(glob("app/*.class.php") as $filepath){
-            $file = explode("/",$filepath);
-            $className = explode(".",$file[count($file)-1]);
-            $className = $className[0];
-            if(strtolower($class)==$className){
-                include $filepath;
-            }
+        if(file_exists('app/'.$class.'.class.php')){
+            include 'app/'.$class.'.class.php';
         }
-        foreach(glob("app/controllers/*.class.php") as $filepath){
-            $file = explode("/",$filepath);
-            $className = explode(".",$file[count($file)-1]);
-            $className = $className[0];
-            if(strtolower($class)==$className){
-                include $filepath;
-            }
+        if(file_exists('app/models/'.$class.'.class.php')){
+            include 'app/models/'.$class.'.class.php';
         }
-        foreach(glob("app/models/*.class.php") as $filepath){
-            $file = explode("/",$filepath);
-            $className = explode(".",$file[count($file)-1]);
-            $className = $className[0];
-            if(strtolower($class)==$className){
-                include $filepath;
-            }
+        if(file_exists('app/controllers/'.$class.'.class.php')){
+            include 'app/controllers/'.$class.'.class.php';
         }
+        // foreach(glob("app/*.class.php") as $filepath){
+        //     $file = explode("/",$filepath);
+        //     $className = explode(".",$file[count($file)-1]);
+        //     $className = $className[0];
+        //     if(strtolower($class)==$className){
+        //         include $filepath;
+        //     }
+        // }
+        // foreach(glob("app/controllers/*.class.php") as $filepath){
+        //     $file = explode("/",$filepath);
+        //     $className = explode(".",$file[count($file)-1]);
+        //     $className = $className[0];
+        //     if(strtolower($class)==$className){
+        //         include $filepath;
+        //     }
+        // }
+        // foreach(glob("app/models/*.class.php") as $filepath){
+        //     $file = explode("/",$filepath);
+        //     $className = explode(".",$file[count($file)-1]);
+        //     $className = $className[0];
+        //     if(strtolower($class)==$className){
+        //         include $filepath;
+        //     }
+        // }
     }
 
     spl_autoload_register('loadClasses');
