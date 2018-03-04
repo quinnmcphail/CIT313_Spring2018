2293e9681dd2cab7aa074b8923bae413ef2a6b62
Quinn McPhail <quinnmcphail@me.com>
2018-03-04 17:04:54 -0500

    Added MVC-Pro

--- a3/views/blog.php
+++ a3/views/blog.php
@@ -1,38 +1,38 @@
 
-<?php include('elements/header.php');?>
+<?php include 'elements/header.php';?>
 <?php
-if( is_array($post) ) {
-	extract($post);?>
+if (is_array($post)) {
+    extract($post);?>
 
 <div class="container">
 	<div class="page-header">
 
-<h1><?php echo $title;?></h1>
+<h1><?php echo $title; ?></h1>
   </div>
-<p><?php echo date("j F Y - g:i:s A",strtotime($date));?></p>
-<p><?php echo $content;?></p>
+<p><?php echo date("j F Y - g:i:s A", strtotime($date)); ?></p>
+<p><?php echo $content; ?></p>
 
 </div>
 <?php }?>
 
-<?php if( is_array($posts) ) {?>
+<?php if (is_array($posts)) {?>
 
 <div class="container">
 <div class="page-header">
 
-<h1><?php echo $title;?></h1>
+<h1><?php echo $title; ?></h1>
   </div>
 
-	<?php foreach($posts as $p){?>
-    <h3><a href="<?php echo BASE_URL?>blog/view/<?php echo $p['pID'];?>" title="<?php echo $p['title'];?>"><?php echo $p['title'];?></a></h3>
-		<sub><?php echo date("j F Y - g:i:s A",strtotime($p['date']));?> by
-		<?php echo $p['userFN']." ".$p['userLN'];?></sub>
-		<sub>Category: <?php echo $p['catName'];?></sub>
-	<p><?php echo $p['content'];?></p>
+	<?php foreach ($posts as $p) {?>
+    <h3><a href="<?php echo BASE_URL ?>blog/view/<?php echo $p['pID']; ?>" title="<?php echo $p['title']; ?>"><?php echo $p['title']; ?></a></h3>
+	<p><?php echo $p['content']; ?></p>
+	<sub><?php echo date("j F Y - g:i:s A", strtotime($p['date'])); ?> by
+		<?php echo $p['userFN'] . " " . $p['userLN']; ?> -
+		Category: <?php echo $p['catName']; ?></sub>
 <?php }?>
 </div>
 
 <?php }?>
 
 
-<?php include('elements/footer.php');?>
+<?php include 'elements/footer.php';?>
