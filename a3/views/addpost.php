98edc30101f915d1eca75e65448c825c4b6bb8c4
Quinn McPhail <quinnmcphail@me.com>
2018-03-04 17:18:15 -0500

    Added MVC-Pro

--- a3/views/addpost.php
+++ a3/views/addpost.php
@@ -15,6 +15,12 @@
         <form action="<?php echo BASE_URL?>addpost/<?php echo $task?>" method="post" onsubmit="editor.post()">
           <label>Title</label>
           <input type="text" class="span6" name="post_title" value="<?php echo $title?>">
+          <label>Date</label>
+          <input type="datetime" name="post_date" value="<?php echo $date?>">
+          <label>Category</label>
+          <select name="post_category">
+          <option>1</option>
+          </select>
      			<label>Content</label>
           <textarea id="tinyeditor" name="post_content" style="width:556px;height: 200px"><?php echo $content?></textarea>
     			<br/>
@@ -22,7 +28,7 @@
           <button id="submit" type="submit" class="btn btn-primary" >Submit</button>
         </form>
 
-        
+
       </div>
     </div>
 </div>
