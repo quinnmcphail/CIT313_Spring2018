872fa748d712b8ed7a073c24dcc44099db32be30
Quinn McPhail <quinnmcphail@me.com>
2018-03-04 16:16:23 -0500

    Added MVC-Pro

--- a3/application/config.php
+++ a3/application/config.php
@@ -1,6 +1,6 @@
 <?php
 define('DEFAULT_VIEW', 'home');//set this to any page to be the default home page
-define('BASE_URL', 'http://corsair.cs.iupui.edu:22231/CIT313/SP2018/a3/');
+define('BASE_URL', 'http://corsair.cs.iupui.edu:22231/CIT313/SP2018/a3');
 
 //database info
 define('DB_HOST', 'localhost');
