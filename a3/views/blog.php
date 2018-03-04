f76e3d75c85651eae085507af21b6d4e492015fe
Quinn McPhail <quinnmcphail@me.com>
2018-03-04 16:58:41 -0500

    Added MVC-Pro

--- a3/views/blog.php
+++ a3/views/blog.php
@@ -27,6 +27,7 @@ if( is_array($post) ) {
     <h3><a href="<?php echo BASE_URL?>blog/view/<?php echo $p['pID'];?>" title="<?php echo $p['title'];?>"><?php echo $p['title'];?></a></h3>
 		<h4><?php echo date("j F Y - g:i:s A",strtotime($p['date']));?></h4>
 		<h4>By: <?php echo $p['userFN']." ".$p['userLN'];?></h4>
+		<h4>Category: <?php echo $p['catName'];?></h4>
 	<p><?php echo $p['content'];?></p>
 <?php }?>
 </div>
