59fbe71b851a222cd74906ffec6c5ed08731990b
Quinn McPhail <quinnmcphail@me.com>
2018-03-04 20:29:00 -0500

    Added registration

--- a3/views/members.php
+++ a3/views/members.php
@@ -1,7 +1,18 @@
 <?php include 'elements/header.php';?>
+<?php if (is_array($user)) {
+    extract($user);?>
 <div class="container">
 	<div class="page-header">
-   <h1> the Members View </h1>
+        <h1><?=$first_name . ' ' . $last_name?></h1>
+    </div>
+    <p>First Name: <?=$first_name?></p>
+    <p>Last Name: <?=$last_name?></p>
+    <p>Email: <?=$email?></p>
+<?php }?>
+<?php if (is_array($users)) {?>
+<div class="container">
+	<div class="page-header">
+   <h1><?=$title?></h1>
   </div>
   <table class="table table-striped">
     <tr>
@@ -9,14 +20,15 @@
         <th>First Name</th>
         <th>Last Name</th>
     </tr>
-    <?php if (is_array($users)) {
-    foreach ($users as $u) {?>
+
+    <?php foreach ($users as $u) {?>
     <tr>
-        <td><?=$u['email'];?></td>
+        <td><a href="<?php echo BASE_URL ?>members/view/<?=$u['uID'];?>"><?=$u['email'];?></a></td>
         <td><?=$u['first_name'];?></td>
         <td><?=$u['last_name'];?></td>
     </tr>
-    <?php }}?>
+    <?php }?>
   </table>
 </div>
+<?php }?>
 <?php include 'elements/footer.php';?>
