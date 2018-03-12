785ca97055fed1ea2766a7a79a917ab3ddfd9532
Quinn McPhail <quinnmcphail@me.com>
2018-03-11 21:14:54 -0400

    Copied files

--- ex4/views/login.php
+++ ex4/views/login.php
@@ -13,13 +13,13 @@
   <div class="control-group">
     <label class="control-label" for="inputEmail">Email</label>
     <div class="controls">
-      <input type="text" id="inputEmail" placeholder="Email">
+      <input type="text" id="inputEmail" name="email" placeholder="Email">
     </div>
   </div>
   <div class="control-group">
     <label class="control-label" for="inputPassword">Password</label>
     <div class="controls">
-      <input type="password" id="inputPassword" placeholder="Password">
+      <input type="password" id="inputPassword" name="password" placeholder="Password">
     </div>
   </div>
   <div class="control-group">
