9e2431010778178c76b68e06e1f4daebee1ad53e
Quinn McPhail <quinnmcphail@me.com>
2018-03-11 21:12:35 -0400

    Copied files

--- ex4/application/load.php
+++ ex4/application/load.php
@@ -1,14 +1,15 @@
 <?php
 
-class Load {
-   function view( $file_name, $data = null ) 
-   {
-      if( is_array($data) ) {
-         extract($data);
-      }
-      include 'views/' . $file_name;
-   }
+class Load
+{
+    public function view($file_name, $data = null)
+    {
+        if (is_array($data)) {
+            extract($data);
+        }
+
+        $u = new User();
+
+        include 'views/' . $file_name;
+    }
 }
-
-
-
