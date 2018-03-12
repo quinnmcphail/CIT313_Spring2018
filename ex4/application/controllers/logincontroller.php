4fe1f96de11f232c9d5c413c9b837305407e11fb
Quinn McPhail <quinnmcphail@me.com>
2018-03-11 21:08:33 -0400

    Copied files

--- ex4/application/controllers/logincontroller.php
+++ ex4/application/controllers/logincontroller.php
@@ -13,8 +13,8 @@ class LoginController extends Controller
     public function do_login()
     {
         $this->userObject = new User();
-        if ($this->userObject->checkUser($_POST['email'], $_POST['password'])) {
-            $userInfo = $this->userObject->getUserFromEmail($_POST['email']);
+        if ($this->userObject->checkUser($_POST['inputEmail'], $_POST['inputPassword'])) {
+            $userInfo = $this->userObject->getUserFromEmail($_POST['inputEmail']);
 
             $_SESSION['uID'] = $userInfo['uID'];
         } else {
