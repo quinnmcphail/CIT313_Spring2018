b8fbdce86c9f4c7e756b6b410627a41fb8edf379
Quinn McPhail <quinnmcphail@me.com>
2018-04-01 18:32:09 -0400

    fixed bug again

---
+++ ex6/application/load.php
@@ -0,0 +1,15 @@
+<?php
+
+class Load
+{
+    public function view($file_name, $data = null)
+    {
+        if (is_array($data)) {
+            extract($data);
+        }
+
+        $u = new User();
+
+        include 'views/' . $file_name;
+    }
+}
---
+++ ex6/views/register.php
@@ -0,0 +1,45 @@
+<?php include 'elements/header.php';?>
+<div class="container">
+	<div class="page-header">
+   <h1>Register</h1>
+  </div>
+  <?php if ($message) {?>
+    <div class="alert alert-success">
+    <button type="button" class="close" data-dismiss="alert">×</button>
+    	<?=$message?>
+    </div>
+  <?php }?>
+   <form class="form-horizontal" action="<?=BASE_URL?>register/<?=$task?>" method="post">
+  <div class="control-group">
+    <label class="control-label" for="inputEmail">Email</label>
+    <div class="controls">
+      <input type="text" id="inputEmail" name="email" placeholder="Email">
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="control-label" for="inputFirstName">First Name</label>
+    <div class="controls">
+      <input type="text" id="inputFirstName" name="firstName" placeholder="First Name">
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="control-label" for="inputFirstName">Last Name</label>
+    <div class="controls">
+      <input type="text" id="inputLastName" name="lastName" placeholder="Last Name">
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="control-label" for="inputPassword">Password</label>
+    <div class="controls">
+      <input type="password" id="inputPassword" name="password" placeholder="Password">
+    </div>
+  </div>
+  <div class="control-group">
+    <div class="controls">
+      <button type="submit" class="btn">Register</button>
+    </div>
+  </div>
+</form>
+</div>
+<?php include 'elements/footer.php';?>
+
