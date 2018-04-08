d3fa420138599f80411d38746a54d6e61fa7e839
Quinn McPhail <quinnmcphail@me.com>
2018-04-08 18:11:52 -0400

    a4

--- a4/application/controllers/homecontroller.php
+++ a4/application/controllers/homecontroller.php
@@ -7,10 +7,9 @@ class HomeController extends Controller
     {
         $rss = new RssDisplay("http://fox59.com/feed");
         $items = $rss->getFeedItems(1);
-        // foreach($items as $item){
-        //     echo($item->title."<br>");
-        // }
-        var_dump($items);
+        foreach($items as $item){
+            echo($item->title."<br>");
+        }
     }
 
 }
