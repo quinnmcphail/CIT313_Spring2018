6c5150ae37ea066dd03c0a9a321b312719411d7b
Quinn McPhail <quinnmcphail@me.com>
2018-04-08 17:43:01 -0400

    a4

--- a4/application/models/rssdisplay.php
+++ a4/application/models/rssdisplay.php
@@ -11,12 +11,13 @@ class RssDisplay extends Model{
 
     public function getFeedItems($num_feed_items){
         $items = simplexml_load_file($this->feed_url);
+        $items = $items->channel->item;
 
         if(!is_null($num_feed_items)){
             $items = array_slice($items,0,$num_feed_items,true);
             $this->num_feed_items = $num_feed_items;
         }
-        return $items->channel->item;
+        return $items;
     }
 
     public function getChannelInfo(){
