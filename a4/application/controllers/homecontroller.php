1485133854f4692b82ae365fb0f00af293034fbf
Quinn McPhail <quinnmcphail@me.com>
2018-04-08 18:16:13 -0400

    a4

--- a4/application/controllers/homecontroller.php
+++ a4/application/controllers/homecontroller.php
@@ -7,9 +7,9 @@ class HomeController extends Controller
     {
         $rss = new RssDisplay("http://fox59.com/feed");
         $items = $rss->getFeedItems(2);
-        // foreach($items as $item){
-        //     echo($item->title."<br>");
-        // }
+        foreach($items as $item){
+            echo($item->title."<br>");
+        }
         var_dump($items);
     }
 
--- a4/application/models/rssdisplay.php
+++ a4/application/models/rssdisplay.php
@@ -15,34 +15,18 @@ class RssDisplay extends Model
     {
         $items = simplexml_load_file($this->feed_url);
         $items = $items->channel->item;
-        $itemsArray = $this->xml2array($items);
 
         // if(!is_null($num_feed_items)){
         //     $itemsArray = array_slice($itemsArray,0,$num_feed_items);
         //     $this->num_feed_items = $num_feed_items;
         // }
 
-        return $itemsArray;
+        return $items;
     }
 
     public function getChannelInfo()
     {
         $channel = simplexml_load_file($this->feed_url);
         return $channel->channel;
-    }
-
-    public function xml2array($xml)
-    {
-        $arr = array();
-
-        foreach ($xml->children() as $r) {
-            $t = array();
-            if (count($r->children()) == 0) {
-                $arr[$r->getName()] = strval($r);
-            } else {
-                $arr[$r->getName()][] = xml2array($r);
-            }
-        }
-        return $arr;
     }
 }
