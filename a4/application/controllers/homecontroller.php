63c53a28609d6a5a67203909dd135068b1db568c
Quinn McPhail <quinnmcphail@me.com>
2018-04-08 19:16:42 -0400

    a4

--- a4/application/controllers/homecontroller.php
+++ a4/application/controllers/homecontroller.php
@@ -6,8 +6,26 @@ class HomeController extends Controller
     public function index()
     {
         $rss = new RssDisplay("http://digitaltrends.com/feed");
-        $this->set("feed_data",$rss->getFeedItems(8));
-        $this->set("channel_info",$rss->getChannelInfo());
+        $this->set("feed_data", $rss->getFeedItems(8));
+        $this->set("channel_info", $rss->getChannelInfo());
+    }
+
+    public function getLocation()
+    {
+        if (!empty($_POST['latitude']) && !empty($_POST['longitude'])) {
+            $url = 'http://maps.googleapis.com/maps/api/geocode/json?latlng=' . trim($_POST['latitude']) . ',' . trim($_POST['longitude']) . '&sensor=false';
+            $json = @file_get_contents($url);
+            $data = json_decode($json);
+            $status = $data->status;
+
+            if ($status == "OK") {
+                $location = $data->results[0]->formatted_address;
+            } else {
+                $location = '';
+            }
+
+            echo $location;
+        }
     }
 
 }
--- a4/views/home/index.php
+++ a4/views/home/index.php
@@ -7,6 +7,7 @@
     </div>
   <?php }?>
 	<div class="page-header">
+  <p>Your Location: <span id="location"></span></p>
     <h1>Articles From <?php echo $channel_info->title; ?></h1>
   </div>
   <?php foreach ($feed_data as $article) {?>
@@ -17,3 +18,29 @@
   <?php }?>
 </div>
 <?php include 'views/elements/footer.php';?>
+<script>
+$(document).ready(function(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showLocation);
+    }else{
+        $('#location').html('Geolocation is not supported by this browser.');
+    }
+});
+
+function showLocation(position){
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    $.ajax({
+        type:'POST',
+        url:'<?php echo BASE_URL; ?>getLocation',
+        data:'latitude='+latitude+'&longitude='+longitude,
+        success:function(msg){
+            if(msg){
+               $("#location").html(msg);
+            }else{
+                $("#location").html('Not Available');
+            }
+        }
+    });
+}
+</script>
