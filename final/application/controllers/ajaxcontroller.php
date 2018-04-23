c26b500b08c377196c926b7f046f77215b0a8bea
Quinn McPhail <quinnmcphail@me.com>
2018-04-22 22:18:02 -0400

    Added ajax comments

--- final/application/controllers/ajaxcontroller.php
+++ final/application/controllers/ajaxcontroller.php
@@ -58,8 +58,7 @@ class AjaxController extends Controller
     public function add_post_comment()
     {
         $this->postObject = new Post();
-        //$_POST['uID'], $_POST['commentText'], $_POST['date'], $_POST['pID']
-        $message = $this->postObject->addComment("Test");
+        $message = $this->postObject->addComment($_POST['uID'], $_POST['commentText'], $_POST['date'], $_POST['pID']);
         $this->set("response", $message);
     }
 }
