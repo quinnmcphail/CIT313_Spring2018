f5308e68e3cad32396cf3f7d068759db9ac1652f
Quinn McPhail <quinnmcphail@me.com>
2018-04-22 23:05:44 -0400

    Added ajax comments

--- final/views/blog/post.php
+++ final/views/blog/post.php
@@ -88,7 +88,7 @@ function refreshComments(){
 						let temp = $(`<hr><p>${e.commentText}<?php if ($u->isAdmin()) {?> <a comment="${e.commentID}" class="btn comment-deleter">Delete Comment</a><?php }?></p><sub>${e.UserFN} ${e.UserLN} commented on ${e.Date}</sub>`);
 						temp.click(function(e){
 							e.preventDefault();
-							console.log(this);
+							console.log($("a",this));
 						});
 					comments.append(temp);
 				});
