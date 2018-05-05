849f76dde0ed18ad2c1a2742481ae3d7f0f85aea
Quinn McPhail <quinnmcphail@me.com>
2018-05-05 16:04:53 -0400

    Final 2

--- final2/views/elements/footer.php
+++ final2/views/elements/footer.php
@@ -7,29 +7,9 @@
 <?php
 if ($u->isAdmin()) {
     ?>
-    <script src="<?php echo BASE_URL ?>application/plugins/tinyeditor/tiny.editor.packed.js"></script>
+    <script src="<?php echo BASE_URL ?>application/plugins/tinymce.min.js"></script>
     <script>
-			var editor = new TINY.editor.edit('editor', {
-				id: 'tinyeditor',
-				width: 584,
-				height: 175,
-				cssclass: 'tinyeditor',
-				controlclass: 'tinyeditor-control',
-				rowclass: 'tinyeditor-header',
-				dividerclass: 'tinyeditor-divider',
-				controls: ['bold', 'italic', 'underline', 'strikethrough', '|', 'subscript', 'superscript', '|',
-					'orderedlist', 'unorderedlist', '|', 'outdent', 'indent', '|', 'leftalign',
-					'centeralign', 'rightalign', 'blockjustify', '|', 'unformat', '|', 'undo', 'redo', 'n',
-					'font', 'size', 'style', '|', 'image', 'hr', 'link', 'unlink', '|'],
-				footer: true,
-				fonts: ['Verdana','Arial','Georgia','Trebuchet MS'],
-				xhtml: true,
-				cssfile: 'custom.css',
-				bodyid: 'editor',
-				footerclass: 'tinyeditor-footer',
-				toggle: {text: 'source', activetext: 'wysiwyg', cssclass: 'toggle'},
-				resize: {cssclass: 'resize'}
-			});
+	tinymce.init({selector:'#tinyeditor'});
 		</script>
  <?php }?>
   </body>
