48db7076e4e313b7187d8ec0a9afa1dd7d3f7aac
Quinn McPhail <quinnmcphail@me.com>
2018-05-05 15:56:19 -0400

    Final 2

--- final2/views/manageposts/add.php
+++ final2/views/manageposts/add.php
@@ -14,20 +14,19 @@
       <div class="span8">
         <form action="<?=BASE_URL?>manageposts/<?=$task?>" method="post" onsubmit="editor.post()">
           <label>Title</label>
-          <input type="text" class="span6" name="post_title" value="<?=$title?>" required>
+          <input type="text" class="span6" name="post_title" required>
           <label>Date</label>
-          <input type="datetime-local" name="post_date" value="<?=date("Y-m-d\TH:i:s", strtotime($date));?>" required>
+          <input type="datetime-local" name="post_date" required>
           <label>Category</label>
           <select name="post_category">
           <?php if (is_array($categories)) {
     foreach ($categories as $c) {?>
-            <option value="<?=$c["categoryID"];?>" <?=$categoryID == $c["categoryID"] ? 'selected' : '';?>><?=$c["name"];?></option>
+            <option value="<?=$c["categoryID"];?>"><?=$c["name"];?></option>
           <?php }}?>
           </select>
      			<label>Content</label>
-          <textarea id="tinyeditor" name="post_content" style="width:556px;height: 200px" required><?=$content?></textarea>
+          <textarea id="tinyeditor" name="post_content" style="width:556px;height: 200px" required></textarea>
     			<br/>
-          <input type="hidden" name="pID" value="<?=$pID?>"/>
           <button id="submit" type="submit" class="btn btn-primary" >Submit</button>
         </form>
 
